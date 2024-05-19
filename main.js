887abb0230b2a31a3e25c7e909b76ad0676e739c
Flora <flora.aubry1994@gmail.com>
2024-05-19 23:27:01 +0200

    Needed cleanup

--- main.js
+++ main.js
@@ -7,47 +7,27 @@ function createWindow() {
         width: 1280,
         height: 750,
         show: false,
+        fullscreen: true,
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
         }
     });
 
     mainWindow.loadURL('https://pokerogue.net/');
-
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.webContents.insertCSS(`
             body, html {
                 cursor: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden; /* Prevent scrollbar */
-                width: 100%;
-                height: 100%;
+                overflow: hidden;
             }
             #app {
                 background: black;
-                display: flex;
-                justify-content: center;
                 align-items: center;
-                height: 100vh; /* Full height viewport */
-                width: 100vw; /* Full width viewport */
-                overflow: hidden; /* Prevent scrollbar */
             }
         `);
 
-        mainWindow.webContents.executeJavaScript(`
-            const appContainer = document.getElementById('app');
-            if (appContainer) {
-                appContainer.style.display = 'flex';
-                appContainer.style.justifyContent = 'center';
-                appContainer.style.alignItems = 'center';
-            }
-        `);
-
-        mainWindow.setMenuBarVisibility(false);
         setupMouseMoveHandler();
-        //setupRequestInterceptor();
-        mainWindow.setFullScreen(true);
         mainWindow.show();
     });
 
@@ -73,29 +53,6 @@ function setupMouseMoveHandler() {
 
         startCursorTimer();
     `);
-}
-
-function setupRequestInterceptor() {
-    const webContents = mainWindow.webContents;
-
-    try {
-        webContents.debugger.attach('1.3');
-    } catch (err) {
-        console.error('Debugger attach failed:', err);
-    }
-
-    webContents.debugger.on('message', (event, method, params) => {
-        if (method === 'Network.responseReceived') {
-            const { response } = params;
-            if (response.url.includes('.mp4')) {
-                mainWindow.setFullScreen(true);
-                mainWindow.show();
-                webContents.debugger.detach();
-            }
-        }
-    });
-
-    webContents.debugger.sendCommand('Network.enable');
 }
 
 app.on('ready', createWindow);
